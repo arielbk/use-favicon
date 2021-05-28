1505b535325e37ef0523729204a382c8eeea801c
arielbk <buchwald.kearney@gmail.com>
2021-05-28 17:53:33 +0200

    feat: add away emoji

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,38 @@ export interface faviconFns {
 }
 
 export interface faviconOptions {
-  icon: string;
+  icon?: string;
+  emoji?: string;
+  awayEmoji?: string;
 }
 
-const useFavicon = ({ icon }: faviconOptions): faviconFns => {
-  const [letterIndex] = useState(0);
+const useFavicon = ({ icon, emoji, awayEmoji }: faviconOptions): faviconFns => {
   const [isNotification, setIsNotification] = useState(true);
+  const [isAway, setIsAway] = useState(false);
 
   useEffect(() => {
     const favicon = document.querySelector("link[rel='icon']");
-    if (favicon) favicon.setAttribute("href", icon);
-  }, [letterIndex, isNotification]);
+    if (!favicon) return;
+    if (icon) favicon.setAttribute("href", icon);
+  }, [isNotification]);
+
+  useEffect(() => {
+    const favicon = document.querySelector("link[rel='icon']");
+    if (!favicon) return;
+    if (emoji) {
+      favicon.setAttribute(
+        "href",
+        `data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">${
+          isAway ? awayEmoji : emoji
+        }</text></svg>`
+      );
+    }
+  }, [isAway]);
+
+  useEffect(() => {
+    const toggleIsAway = () => setIsAway((prev) => !prev);
+    document.addEventListener("visibilitychange", toggleIsAway);
+  }, []);
 
   return {
     triggerNotification: useCallback(() => setIsNotification(true), []),
