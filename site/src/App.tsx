843a91af6d70455a7ad64eb82a4d18feae909ba2
arielbk <buchwald.kearney@gmail.com>
2022-11-26 19:29:42 +0100

    Display favicon with variants

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,13 +1,58 @@
+import { RadioGroup, Stack, Radio, Button } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 import useFavicon from 'use-favicon';
+import { FaviconOptions } from 'use-favicon/dist/types';
+import FaviconViewer from './components/FaviconViewer';
+
+type Variant = 'default' | 'dark' | 'away';
+
+const defaultOptions: FaviconOptions = {
+  type: 'emoji',
+  value: '🙂',
+  darkVariant: {
+    type: 'emoji',
+    value: '✨',
+  },
+  awayVariant: {
+    type: 'emoji',
+    value: '🫣',
+  },
+};
 
 function App() {
-  const { faviconSvg } = useFavicon();
+  const [wholeOptions, setWholeOptions] =
+    useState<FaviconOptions>(defaultOptions);
+  const [selectedVariant, setSelectedVariant] = useState<Variant>('default');
+
+  const { faviconSvg, setOptions, setFaviconNotification } =
+    useFavicon(defaultOptions);
+
+  useEffect(() => {
+    if (selectedVariant === 'default') {
+      setOptions(wholeOptions);
+    } else if (selectedVariant === 'dark') {
+      setOptions({ ...wholeOptions, ...wholeOptions.darkVariant });
+    } else if (selectedVariant === 'away') {
+      setOptions({ ...wholeOptions, ...wholeOptions.awayVariant });
+    }
+  }, [wholeOptions, selectedVariant]);
 
   return (
     <div>
-      {faviconSvg ? (
-        <img src={`data:image/svg+xml,${faviconSvg}`} width={64} height={64} />
-      ) : null}
+      <FaviconViewer faviconSvg={faviconSvg ?? null} />
+      <RadioGroup
+        onChange={(newVal: Variant) => setSelectedVariant(newVal)}
+        value={selectedVariant}
+      >
+        <Stack direction="row">
+          <Radio value="default">Default</Radio>
+          <Radio value="dark">Dark</Radio>
+          <Radio value="away">Away</Radio>
+        </Stack>
+      </RadioGroup>
+      <Button onClick={() => setFaviconNotification()}>
+        Toggle notification
+      </Button>
     </div>
   );
 }
